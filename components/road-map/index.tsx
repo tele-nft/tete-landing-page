1926e17acd6dafe9645b7ae98ce8d52b53c5f779
thanhpv <v.thanhpv7@vinid.net>
2021-10-12 00:14:09 +0700

    Update code roadmap

--- components/road-map/index.tsx
+++ components/road-map/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import Image from "next/image";
 
-import Title from "../title";
 import Styles from "./roadmap.module.css";
 export default function RoadMap() {
   return (
@@ -13,8 +12,7 @@ export default function RoadMap() {
             src={require("public/images/roadmap.png")}
             alt="tw"
             // layout="fill"
-            // objectFit="cover"
-            // quality={100}
+            quality={100}
           />
         </Row>
       </Container>
