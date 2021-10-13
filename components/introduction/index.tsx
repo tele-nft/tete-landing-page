3fdd32f3b2bc11c2c7605f71134d9e2e87e08a5c
alvin.nguyen <alvin.nguyen@luminetsoft.com>
2021-10-13 09:03:59 +0700

    remove connect button

--- components/introduction/index.tsx
+++ components/introduction/index.tsx
@@ -14,18 +14,6 @@ export default function Introduction() {
                         GunHunter is an NFT game and metaverses ecosystem that intends to create a Universe of Fighting between players on
                         Binance Smart Chains, Polygon, Solona, Polkadot, ...where millions of players from all over the world.
                     </p>
-                    <div className={`d-flex align-items-center ${Styles.connect}`}>
-                        <a href="#">
-                            <button className={Styles.button}>
-                                <img src="./images/connect.svg" alt="connect" />
-                            </button>
-                        </a>
-                        <a href="#">
-                            <button className={Styles.button}>
-                                <img src="./images/giveaway.svg" alt="giveaway" />
-                            </button>
-                        </a>
-                    </div>
                     <img src="./images/thumbnail.jpg" alt="thumbnail" />
                 </Row>
             </Container>
