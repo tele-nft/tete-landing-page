c312fddd2c7195f53ef9c0815e3f88eb514965bc
thanhpv1991 <thanh299255@gmail.com>
2021-10-18 01:01:04 +0700

    Update code

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
-import Styles from './footer.module.css';
+import Styles from "./footer.module.css";
 
 export default function Footer() {
   return (
@@ -9,19 +9,55 @@ export default function Footer() {
       <div className="section black">
         <div className="container">
           <div className="div-block-2">
-            <p data-w-id="cce639c3-4147-fc63-0e56-76b5e528e62c" className={Styles.follow}>
+            <p
+              data-w-id="cce639c3-4147-fc63-0e56-76b5e528e62c"
+              className={Styles.follow}
+            >
               Follow us on
             </p>
-            <div data-w-id="b80a8957-8d10-2ff8-6e29-17f4a62bedea" className="divsocialicons">
-              <a href="http://t.me/GunHunterNftOfficial " target="_blank" rel="noreferrer" className={Styles.social}>
-                <img src="/images/legal/telegram-image.svg" loading="lazy" alt="" className="iconsocial" />
+            <div
+              data-w-id="b80a8957-8d10-2ff8-6e29-17f4a62bedea"
+              className="divsocialicons"
+            >
+              <a
+                href="http://t.me/GunHunterNftOfficial "
+                target="_blank"
+                rel="noreferrer"
+                className={Styles.social}
+              >
+                <img
+                  src="/images/legal/telegram-image.svg"
+                  loading="lazy"
+                  alt=""
+                  className="iconsocial"
+                />
               </a>
 
-              <a href="https://discord.gg/9hVZWjMK6y" target="_blank" rel="noreferrer" className={Styles.social}>
-                <img src="/images/legal/discord-image.svg" loading="lazy" alt="" className="iconsocial" />
+              <a
+                href="https://discord.gg/9hVZWjMK6y"
+                target="_blank"
+                rel="noreferrer"
+                className={Styles.social}
+              >
+                <img
+                  src="/images/legal/discord-image.svg"
+                  loading="lazy"
+                  alt=""
+                  className="iconsocial"
+                />
               </a>
-              <a href="https://twitter.com/GunHunterNFT" target="_blank" rel="noreferrer" className={Styles.social}>
-                <img src="/images/legal/twitter-image.svg" loading="lazy" alt="" className="iconsocial" />
+              <a
+                href="https://twitter.com/GunHunterNFT"
+                target="_blank"
+                rel="noreferrer"
+                className={Styles.social}
+              >
+                <img
+                  src="/images/legal/twitter-image.svg"
+                  loading="lazy"
+                  alt=""
+                  className="iconsocial"
+                />
               </a>
               <a
                 href="https://www.facebook.com/GunHunter-NFT-102296442234796"
@@ -29,7 +65,12 @@ export default function Footer() {
                 rel="noreferrer"
                 className={Styles.social}
               >
-                <img src="/images/legal/facebook-image.svg" loading="lazy" alt="" className="iconsocial" />
+                <img
+                  src="/images/legal/facebook-image.svg"
+                  loading="lazy"
+                  alt=""
+                  className="iconsocial"
+                />
               </a>
               <a
                 href="https://www.youtube.com/channel/UCZBYirzX--pcuGrG69R6XwA"
@@ -37,10 +78,25 @@ export default function Footer() {
                 rel="noreferrer"
                 className={Styles.social}
               >
-                <img src="/images/legal/youtube-image.svg" loading="lazy" alt="" className="iconsocial" />
+                <img
+                  src="/images/legal/youtube-image.svg"
+                  loading="lazy"
+                  alt=""
+                  className="iconsocial"
+                />
               </a>
-              <a href="http://t.me/GunHunterChannel" target="_blank" rel="noreferrer" className={Styles.social}>
-                <img src="/images/legal/instagram-image.svg" loading="lazy" alt="" className="iconsocial" />
+              <a
+                href="http://t.me/GunHunterChannel"
+                target="_blank"
+                rel="noreferrer"
+                className={Styles.social}
+              >
+                <img
+                  src="/images/legal/instagram-image.svg"
+                  loading="lazy"
+                  alt=""
+                  className="iconsocial"
+                />
               </a>
               <a
                 href="https://gunhunternft.gitbook.io/whitepaper"
@@ -48,12 +104,23 @@ export default function Footer() {
                 rel="noreferrer"
                 className={Styles.social}
               >
-                <img src="/images/legal/m-image.svg" loading="lazy" alt="" className="iconsocial" />
+                <img
+                  src="/images/legal/m-image.svg"
+                  loading="lazy"
+                  alt=""
+                  className="iconsocial"
+                />
               </a>
             </div>
           </div>
-          <div data-w-id="cce639c3-4147-fc63-0e56-76b5e528e636" className="delimitationbar"></div>
-          <div data-w-id="cce639c3-4147-fc63-0e56-76b5e528e637" className={Styles.submenu}>
+          <div
+            data-w-id="cce639c3-4147-fc63-0e56-76b5e528e636"
+            className="delimitationbar"
+          ></div>
+          <div
+            data-w-id="cce639c3-4147-fc63-0e56-76b5e528e637"
+            className={Styles.submenu}
+          >
             <Link href="/privace-policy">
               <a className={Styles.link}>Terms of Service</a>
             </Link>
@@ -62,7 +129,9 @@ export default function Footer() {
             </Link>
           </div>
           <div>
-            <p className={Styles.textCopy}>Copyright &#9400; 2021 by GunHunterNFT </p>
+            <p className={Styles.textCopy}>
+              Copyright &#9400; 2021 by GunHunterNFT{" "}
+            </p>
           </div>
         </div>
       </div>
