699d70813027cd71ab10bfb007894eb40cdb949c
thanhpv1991 <thanh299255@gmail.com>
2021-10-24 21:07:43 +0700

    edit text whitepaper

--- components/heros/character-slider.tsx
+++ components/heros/character-slider.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import Carousel from 'react-material-ui-carousel';
+import React, { useState } from "react";
+import Carousel from "react-material-ui-carousel";
 
-import Character from './character';
-import Styles from './heros.module.css';
-import CharacterSelection from './character-selection';
+import Character from "./character";
+import Styles from "./heros.module.css";
+import CharacterSelection from "./character-selection";
 
 export default function CharacterSlider({ data }: { data: any }) {
   const [select, setSelect] = useState(0);
@@ -19,7 +19,11 @@ export default function CharacterSlider({ data }: { data: any }) {
   return (
     <div className={Styles.characterSlider}>
       <CharacterSelection {...{ data, select, handleSelect }} />
-      <Carousel className={Styles.carousel} index={select} onChange={handleChange} autoPlay={false}>
+      <Carousel
+        className={Styles.carousel}
+        index={select}
+        onChange={handleChange}
+      >
         {data.map((item: any, key: number) => (
           <Character key={key} {...{ ...item }} />
         ))}
