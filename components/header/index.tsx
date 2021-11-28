685a551192eb8e8cbaa11ed2e89c3498c75385bf
MinhDat <haminhdat01@gmail.com>
2021-11-28 16:25:22 +0700

    update presale link

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,76 +1,93 @@
-import React, { useEffect, useState } from "react";
-import { Collapse, Container, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from "reactstrap";
+import React, { useEffect, useState } from 'react';
+import {
+  Collapse,
+  Container,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink
+} from 'reactstrap';
 
-import Styles from "./Header.module.css";
+import Styles from './Header.module.css';
 
 export default function Header({ baseURL = '' }) {
-    const [isOpen, setIsOpen] = useState(false);
-    const [sticky, setSticky] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [sticky, setSticky] = useState(false);
 
-    const toggle = () => setIsOpen(!isOpen);
+  const toggle = () => setIsOpen(!isOpen);
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-    });
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  });
 
-    const handleScroll = () => {
-        if (window.scrollY > 90) {
-            setSticky(true);
-        } else if (window.scrollY < 90) {
-            setSticky(false);
-        }
-    };
+  const handleScroll = () => {
+    if (window.scrollY > 90) {
+      setSticky(true);
+    } else if (window.scrollY < 90) {
+      setSticky(false);
+    }
+  };
 
-    return (
-        <header className={`${Styles.header} ${sticky ? Styles.sticky : ''}`}>
-            <Navbar light expand="md" className={Styles.navbar} fixed="top">
-                <Container>
-                    <NavbarBrand href="/" className={Styles.brand}>
-                        <img className={Styles.logo} src="/images/logo_text.png" alt="logo" />
-                    </NavbarBrand>
-                    <NavbarToggler onClick={toggle} />
-                    <Collapse isOpen={isOpen} navbar>
-                        <Nav className="m-auto" navbar>
-                            <NavItem className={Styles['nav-item']}>
-                                <NavLink className={Styles['nav-link']} href={`${baseURL}#introduction`}>
-                                    Introduction
-                                </NavLink>
-                            </NavItem>
-                            <NavItem className={Styles['nav-item']}>
-                                <NavLink className={Styles['nav-link']} href={`${baseURL}#ecosystem`}>
-                                    Ecosystem
-                                </NavLink>
-                            </NavItem>
-                            <NavItem className={Styles['nav-item']}>
-                                <NavLink className={Styles['nav-link']} href={`${baseURL}#features`}>
-                                    Features
-                                </NavLink>
-                            </NavItem>
-                            <NavItem className={Styles['nav-item']}>
-                                <NavLink className={Styles['nav-link']} href={`${baseURL}#characters`}>
-                                    Characters
-                                </NavLink>
-                            </NavItem>
-                            <NavItem className={Styles['nav-item']}>
-                                <NavLink className={Styles['nav-link']} href={`${baseURL}#token-metrics`}>
-                                    Token Metrics
-                                </NavLink>
-                            </NavItem>
-                            <NavItem className={Styles['nav-item']}>
-                                <NavLink className={Styles['nav-link']} href={`${baseURL}#road-map`}>
-                                    Road Map
-                                </NavLink>
-                            </NavItem>
-
-                            <NavItem className={Styles['nav-item']}>
-                                <NavLink className={Styles['nav-link']} href="https://play.gunhunternft.net/">
-                                    Play Now
-                                </NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Container>
-            </Navbar>
-        </header>
-    );
+  return (
+    <header className={`${Styles.header} ${sticky ? Styles.sticky : ''}`}>
+      <Navbar light expand="md" className={Styles.navbar} fixed="top">
+        <Container>
+          <NavbarBrand href="/" className={Styles.brand}>
+            <img className={Styles.logo} src="/images/logo_text.png" alt="logo" />
+          </NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="m-auto" navbar>
+              <NavItem className={Styles['nav-item']}>
+                <NavLink className={Styles['nav-link']} href={`${baseURL}#introduction`}>
+                  Introduction
+                </NavLink>
+              </NavItem>
+              <NavItem className={Styles['nav-item']}>
+                <NavLink className={Styles['nav-link']} href={`${baseURL}#ecosystem`}>
+                  Ecosystem
+                </NavLink>
+              </NavItem>
+              <NavItem className={Styles['nav-item']}>
+                <NavLink className={Styles['nav-link']} href={`${baseURL}#features`}>
+                  Features
+                </NavLink>
+              </NavItem>
+              <NavItem className={Styles['nav-item']}>
+                <NavLink className={Styles['nav-link']} href={`${baseURL}#characters`}>
+                  Characters
+                </NavLink>
+              </NavItem>
+              <NavItem className={Styles['nav-item']}>
+                <NavLink className={Styles['nav-link']} href={`${baseURL}#token-metrics`}>
+                  Token Metrics
+                </NavLink>
+              </NavItem>
+              <NavItem className={Styles['nav-item']}>
+                <NavLink className={Styles['nav-link']} href={`${baseURL}#road-map`}>
+                  Road Map
+                </NavLink>
+              </NavItem>
+              <NavItem className={Styles['nav-item']}>
+                <NavLink
+                  className={Styles['nav-link']}
+                  target="_blank"
+                  href="https://www.pinksale.finance/#/launchpad/0xe84066bd47e6c22976D32Ff93e7077D3E92d111c?chain=BSC"
+                >
+                  Presale
+                </NavLink>
+              </NavItem>
+              <NavItem className={Styles['nav-item']}>
+                <NavLink className={Styles['nav-link']} href="https://play.gunhunternft.net/">
+                  Play Now
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 }
