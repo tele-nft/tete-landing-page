4a2afdbdf9a3b59c05b7dafdd837f1ae7f8b4b16
alvin.nguyen <alvin.nguyen@luminetsoft.com>
2024-06-03 21:34:03 +0700

    token metrics

--- components/token-metrics/index.tsx
+++ components/token-metrics/index.tsx
@@ -1,9 +1,37 @@
+import { ArcElement, Chart as ChartJS, Legend, Tooltip } from "chart.js";
 import Title from "components/title";
-import Image from "next/image";
 import React from "react";
+import { Doughnut } from "react-chartjs-2";
 import { Container, Row } from "reactstrap";
 
 import Styles from "./token-metrics.module.css";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const data = {
+  labels: [
+    "Fairlaunch",
+    "Dexs Listing",
+    "Marketing",
+    "Reward and Staking",
+    "Team",
+    "Airdrop",
+  ],
+  datasets: [
+    {
+      label: "%",
+      data: [40, 20, 12, 20, 6, 2],
+      backgroundColor: [
+        "rgb(17,242,48)",
+        "rgb(237,90,209)",
+        "rgb(232,242,28)",
+        "rgb(244,144,34)",
+        "rgb(244,25,56)",
+        "rgb(38,105,239)",
+      ],
+    },
+  ],
+};
 
 export default function TokenMetrics() {
   return (
@@ -13,7 +41,12 @@ export default function TokenMetrics() {
           <Title text="TOKENNOMICS" />
         </Row>
         <Row>
-          <Image src={require("public/images/token.png")} alt="token" />
+          <div className={Styles.chart}>
+            <Doughnut
+              data={data}
+              options={{ plugins: { legend: { position: "bottom" } } }}
+            />
+          </div>
         </Row>
       </Container>
     </section>
